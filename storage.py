51d1fb4683661c994f88d38defef2612f1729227
ishaandange67 <120133387+ishaandange67@users.noreply.github.com>
2025-12-06 22:14:15 -0500

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,21 +1,10 @@
-"""
-Storage utilities for loading and saving JSON files.
-"""
+
 import json
 import os
 
 
 def load_json(filename, default=None):
-    """
-    Load data from a JSON file.
-    
-    Args:
-        filename (str): Path to the JSON file.
-        default: Default value if file doesn't exist or is invalid.
-    
-    Returns:
-        The loaded JSON data, or the default value if file doesn't exist.
-    """
+  
     if not os.path.exists(filename):
         return default if default is not None else {}
     
@@ -27,12 +16,6 @@ def load_json(filename, default=None):
 
 
 def save_json(filename, data):
-    """
-    Save data to a JSON file.
-    
-    Args:
-        filename (str): Path to the JSON file.
-        data: Data to save as JSON.
-    """
+  
     with open(filename, 'w') as f:
         json.dump(data, f, indent=2)
